bf69d086b76836c3b6ddd139fc80a893a18d97a6
mikezks <23002661+mikezks@users.noreply.github.com>
2022-06-20 13:01:19 +0200

    feat: testing

--- apps/starter/src/app/flight-booking/features/flight-search/flight-search.component.spec.ts
+++ apps/starter/src/app/flight-booking/features/flight-search/flight-search.component.spec.ts
@@ -1,24 +1,91 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, Directive, EventEmitter, Input, Output, Pipe, PipeTransform } from '@angular/core';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { Flight } from '../../../entities/flight';
+import { FlightService } from '../../data-access/flight.service';
 import { FlightSearchComponent } from './flight-search.component';
 
-describe('FlightSearchComponent', () => {
+
+describe('Unit test: flight-search.component', () => {
   let component: FlightSearchComponent;
   let fixture: ComponentFixture<FlightSearchComponent>;
+  const result = [
+    { id: 17, from: 'Graz', to: 'Hamburg', date: 'now', delayed: true },
+    { id: 18, from: 'Vienna', to: 'Barcelona', date: 'now', delayed: true },
+    { id: 19, from: 'Frankfurt', to: 'Salzburg', date: 'now', delayed: true },
+  ];
+
+  const flightServiceMock = {
+    find(): Observable<Flight[]> {
+      return of(result);
+    },
+    // Implement the following members only if this code is used in your Component
+    flights: [],
+    load(): void {
+      this.find().subscribe(f => { (this.flights as Flight[]) = f; });
+    }
+  };
+
+  // eslint-disable-next-line @angular-eslint/component-selector
+  @Component({ selector: 'flight-card', template: '' })
+  class FlightCardComponent {
+    @Input() item: Flight | undefined;
+    @Input() selected = false;
+    @Output() selectedChange = new EventEmitter<boolean>();
+  }
+
+  // eslint-disable-next-line @angular-eslint/directive-selector
+  @Directive({ selector: 'input[city]' })
+  class CityValidatorDirective {
+    @Input() city: string[] = [];
+    validate = () => null;
+  }
+
+  @Pipe({ name: 'city' })
+  class CityPipe implements PipeTransform {
+    transform = (v: unknown) => v;
+  }
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [FlightSearchComponent],
-    }).compileComponents();
-  });
+      imports: [
+        FormsModule
+      ],
+      declarations: [
+        FlightSearchComponent,
+        FlightCardComponent,
+        CityPipe,
+        CityValidatorDirective
+      ],
+      providers: [
+        {
+          provide: FlightService,
+          useValue: flightServiceMock
+        }
+      ]
+    })
+    .compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(FlightSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should not load flights w/o from and to', () => {
+    component.from = '';
+    component.to = '';
+    component.search();
+
+    expect(component.flights.length).toBe(0);
+  });
+
+  it('should load flights w/ from and to', () => {
+      component.from = 'Hamburg';
+      component.to = 'Graz';
+      component.search();
+
+      expect(component.flights.length).toBeGreaterThan(0);
   });
 });
