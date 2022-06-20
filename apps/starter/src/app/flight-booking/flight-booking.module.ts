06bb3a684dea6f517871780e8144b452afefe961
mikezks <23002661+mikezks@users.noreply.github.com>
2022-06-20 11:00:05 +0200

    feat: routing feature module

--- apps/starter/src/app/flight-booking/flight-booking.module.ts
+++ apps/starter/src/app/flight-booking/flight-booking.module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
-import { FlightSearchComponent } from './features/flight-search/flight-search.component';
-import { FlightCardComponent } from './ui/flight-card/flight-card.component';
 import { FlightEditComponent } from './features/flight-edit/flight-edit.component';
-import { RouterModule } from '@angular/router';
+import { FlightSearchComponent } from './features/flight-search/flight-search.component';
+import { FlightBookingRoutingModule } from './flight-booking-routing.module';
+import { FlightCardComponent } from './ui/flight-card/flight-card.component';
 
 
 @NgModule({
@@ -15,12 +15,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule,
+    FlightBookingRoutingModule,
     SharedModule
-  ],
-  exports: [
-    FlightSearchComponent,
-    FlightEditComponent
-  ],
+  ]
 })
 export class FlightBookingModule {}
